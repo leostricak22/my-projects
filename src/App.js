b167fd1492c7941ba9641f6891adebf8a73bcf45
leostricak22 <stricak.leo@gmail.com>
2024-10-16 11:46:59 +0200

    feat: Query params implementation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import Menu from "./components/Menu";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import ProjectInfo from "./components/ProjectComponent";
 
 function App() {
     const [selectedProject, setSelectedProject] = useState(null);
+
+    useEffect(() => {
+        const path = window.location.pathname;
+        let lastSegment = path.substring(path.lastIndexOf('/') + 1).replaceAll("%20", " ");
+
+        setSelectedProject(lastSegment);
+    }, []);
+
+    useEffect(() => {
+        if(selectedProject !== null)
+            window.history.pushState(null, "", `/${selectedProject}`);
+    }, [selectedProject]);
 
     return (
         <div id="main">
